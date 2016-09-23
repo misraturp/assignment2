47c5e527d87f108aaf4820d994293533620f2215
Alexander Speicher <alex.speicher@gmx.net>
2016-09-23 14:01:37 +0200

    inital commit

---
+++ suspicious.outage/src/main/java/lsde10/suspicious/outage/App.java
@@ -0,0 +1,13 @@
+package lsde10.suspicious.outage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
