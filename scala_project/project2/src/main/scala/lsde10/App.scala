ab76cdf12760dd66d87fabaf53b88807f6070474
Mısra Turp <misraturp@sabanciuniv.edu>
2016-10-27 14:46:19 +0200

    new version

--- scala_project/project2/src/main/scala/lsde10/App.scala
+++ scala_project/project2/src/main/scala/lsde10/App.scala
@@ -25,13 +25,13 @@ object App {
 
   def main(args : Array[String]) {
   
-	def decode (line: String) : AisMessage = {
+	def decode (line: String) : AisPositionMessage = {
 		try{
 			val vdm = new Vdm()
 			vdm.parse(line)
 			val msg = AisMessage.getInstance(vdm)
 			if(msg.isInstanceOf[AisPositionMessage])
-				return msg
+				return msg.asInstanceOf[AisPositionMessage]
 			else 
 				return null
 		}
@@ -53,84 +53,79 @@ object App {
 		
 		var str = strday+strhour
 		return str
-	
 	}
-
 
 	val conf = new SparkConf().setAppName("Finding Outage")
 	val sc = new SparkContext(conf)
 	
-	val text = sc.wholeTextFiles("/user/hannesm/lsde/ais/10/01/*.txt.gz").values.flatMap(file => {
+	var path :String = "/user/hannesm/lsde/ais/10/01/*.txt.gz\n/user/hannesm/lsde/ais/10/02/*.txt.gz\n/user/hannesm/lsde/ais/10/03/*.txt.gz\n/user/hannesm/lsde/ais/10/04/*.txt.gz\n"
+						
+	  val text = sc.wholeTextFiles(path).flatMapValues(y => y.split("\n")).values.flatMap(file => {
 	  val lines = file.split("\n")
 	  val id = lines.head.split(" ").head
 	  lines.tail.map((id, _))
 	})
+
+	text.cache()
 	
 	//decode and filter
-	val data1 = text.filter(p => p._2.contains("!"))
-	val data2 = data1.map(p => (p._1, decode(p._2.substring(p._2.indexOf("!")))))
-	val decoded = data2.filter(p => if(p._2 == null) false else true)
-	
+	val decoded = text.filter(p => p._2.contains("!"))
+						.map(p => (p._1, decode(p._2.substring(p._2.indexOf("!")))))
+						.filter(p => if(p._2 == null) false else true)
+	text.unpersist()
+	decoded.cache()
 	
 	//####################Compute the outages########################################################
-	//location = ((mmsi,timestamp), GeoLocation) 
-	val location = decoded.map(p => ((p._2.getUserId(),p._1.substring(0,p._1.indexOf(".")).toLong), p._2.asInstanceOf[AisPositionMessage].getPos().getGeoLocation()))
-	location.cache()
 	
+	//allShipsLocation = ((mmsi,timestamp), GeoLocation) 
 	//Check if GeoLocation is null
-	val data5 = location.filter(p => if(p._2 == null) false else true)
+	
+	val allShipsLocation = decoded.map(p => ((p._2.getUserId(),p._1.substring(0,p._1.indexOf(".")).toLong), p._2.getPos().getGeoLocation()))
+							.filter(p => if(p._2 == null) false else true)
+	decoded.unpersist()
+	allShipsLocation.cache()
 	
 	//get the distinct values
-	val distinct = data5.map(kv => (kv._1,kv)).reduceByKey {case (a,b) => a}.map(_._2)
-
 	//sort by (mmsi,timestamp)
-	val sorted = distinct.sortBy(_._1)
-	
 	//get the gaps, the resulting type is (mmsi,gap,starttime, endtime, lat,long)
 	//both latitude and longitude are truncated to reflect the "area"
 	//key is here (mmsi,timestamp)
-	var data = sorted.sliding(2).collect({case Array((key1, val1), (key2, val2)) if key1._1 == key2._1 => (key1._1, key2._2 - key1._2, getTimeKey(new DateTime(key1._2*1000).toDateTime), getTimeKey(new DateTime(key2._2*1000).toDateTime), (math floor val1.getLatitude() *10)/10, (math floor val1.getLongitude() *10)/10)})
-	//data.cache()
- 
 	//check if mmsi number is correct
-	var digitCheck = data.filter(p => if(p._1.toString.length==9)true else false)
-	
 	//gap interval between 20 mins to 10 hours
-	var reduced = digitCheck.filter(p => if(p._2 > 1200 && p._2 < 36000) true else false)
 	
-	//var reduced = data.reduceByKey(Math.max(_, _))	
-	reduced.saveAsTextFile("reduced_solutions")	
-	
-	
+	var outageData = allShipsLocation.map(kv => (kv._1,kv)).reduceByKey {case (a,b) => a}.map(_._2)
+							.sortBy(_._1)
+							.sliding(2).collect({case Array((key1, val1), (key2, val2)) if key1._1 == key2._1 => (key1._1, key2._2 - key1._2, getTimeKey(new DateTime(key1._2*1000).toDateTime), getTimeKey(new DateTime(key2._2*1000).toDateTime), (math floor val1.getLatitude() *10)/10, (math floor val1.getLongitude() *10)/10)})
+							.filter(p => if(p._1.toString.length==9)true else false)
+							.filter(p => if(p._2 > 1200 && p._2 < 36000) true else false)
+	outageData.cache()	
+							
 	//####################Connectivity for area########################################################
-	
-	def calculatePercentage(sendingShips: Int, gappingShips: Int) : Float = {
-		val percentage = (100*gappingShips.toFloat)/sendingShips.toFloat
-		return percentage	
-	}
 	
 	//************transmitting ships****************
 	//get Location information : format = ((lat,long,timekey), mmsi)
-	var geo1 = data5.map(p => (((math floor p._2.getLatitude() *10)/10, (math floor p._2.getLongitude() *10)/10, getTimeKey(new DateTime(p._1._2*1000).toDateTime)),p._1._1))
 	//get number of ships sending in the area and timeinterval
-	var geo2 = geo1.distinct().groupByKey().map(p => (p._1,p._2.size))
+	var allAreas = allShipsLocation.map(p => (((math floor p._2.getLatitude() *10)/10, (math floor p._2.getLongitude() *10)/10, getTimeKey(new DateTime(p._1._2*1000).toDateTime)),p._1._1))
+									.distinct()
+									.groupByKey().map(p => (p._1,p._2.size))
+	allShipsLocation.unpersist()
+	allAreas.cache()
 	
 	//***********not transmitting ships*****************
 	//same idea, just for getting the number of ships that have the start of gaps in that place (there will be overlaps)
-	var geogap = reduced.map(p => ((p._5,p._6,p._3),p._1))
-	var geogap2 = geogap.distinct().groupByKey().map(p => (p._1,p._2.size))
+	var nonTransmittingAreas = outageData.map(p => ((p._5,p._6,p._3),p._1))
+										.distinct()
+										.groupByKey().map(p => (p._1,p._2.size))
+	nonTransmittingAreas.cache()
+	
 	//((lat,lon,timekey),(size1,size2))
-	
 	//join the two rdds to calculate the connectivity
-	var result = geo2.join(geogap2)
 	//((lat,lon,timekey),percentage)
-	var result1 = result.map(p => (p._1,(calculatePercentage(p._2._1,p._2._2))))
-	//var percentages = result1.filter(p => if (p._2 <= 20) true else false)
-	
-	//save to hdfs
-	//geo2.saveAsTextFile("geo_solutions")
-	result.saveAsTextFile("joined_tables")
-	result1.saveAsTextFile("percentages")
+	var result = allAreas.join(nonTransmittingAreas)
+							.map(p => (p._1,(100*p._2._2.toFloat)/p._2._1.toFloat))
+	allAreas.unpersist()
+	nonTransmittingAreas.unpersist()
+	result.cache()
 	
 	//#################Ranking Outages######################################################
 	
@@ -162,19 +157,24 @@ object App {
 			
 	}
 	
-	//reduced(mmsi, gap, starttime, endtime, lat, long) gaps of all ships
-	//result1((lat,lon,timekey),percentage)
+	//outageData(mmsi, gap, starttime, endtime, lat, long) gaps of all ships
+	//result((lat,lon,timekey),percentage)
 	//what we want to have (mmsi, gap, geo_percentage, starttime)
 	//percentage = percentage of ships that have an outage
 	
-	val gapShips = reduced.map(p => ((p._5,p._6,p._3),(p._1,p._2))) //((lat,lon,startime),(mmsi,gap))
-	val joinedGapShips = gapShips.join(result1)	//((lat,lon,starttime),((mmsi,gap),percentage))
-	val rankingReady = joinedGapShips.map(p => (p._2._1._1, p._2._1._2, p._2._2, p._1._3)) //(mmsi, gap, geo_percentage, starttime)
-	val rankedShips = rankingReady.map(p => (p._1,rankShip(p._2, p._3, p._4)))	//(mmsi, rank)
+	//((lat,lon,startime),(mmsi,gap))
+	val gapShips = outageData.map(p => ((p._5,p._6,p._3),(p._1,p._2))) 
 	
+	//((lat,lon,starttime),((mmsi,gap),percentage)) - result of join
+	//(mmsi, rank) - final product
+	val rankedShips = gapShips.join(result)
+					.map(p => (p._2._1._1,rankShip(p._2._1._2, p._2._2, p._1._3)))	
+					.filter(p => p._2 > 50)
+									
+	outageData.unpersist()
+	result.unpersist()
 	
-	rankedShips.saveAsTextFile("ranked_ships")
+	rankedShips.saveAsTextFile("result")
 	
-  }
-
+	}
 }
